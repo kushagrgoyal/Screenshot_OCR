0a6fe05810f705720f2525df046388c1079b2558
kushagrgoyal <kushagrgoyal@gmail.com>
2022-03-11 00:28:03 +0530

    Horizontal and vetical lines for cursor working

--- screenshotter.py
+++ screenshotter.py
@@ -1,3 +1,4 @@
+import copy
 import cv2
 import numpy as np
 from PIL import ImageGrab
@@ -38,15 +39,51 @@ class MainWindow(qtw.QWidget):
         img = pyautogui.screenshot()
         self.show()
         img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        img_lines = copy.deepcopy(img) # Copying the image in a duplicate variable name
         
         # Creating a cv2 window and setting full-screen property to it
         cv2.namedWindow('Screenshot', cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty('Screenshot', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        cv2.imshow('Screenshot', img)
+        # cv2.imshow('Screenshot', img)
 
+        
+        # def mouse_move(event):
+        #     if event == cv2.EVENT_MOUSEMOVE:
+        #         while True:
+        #             # Horizontal line
+        #             cv2.line(img_lines, (0, pyautogui.position()[1]), (size_x, pyautogui.position()[1]), (0, 255, 0), 1)
+
+        #             # Vertical Line
+        #             cv2.line(img_lines, (pyautogui.position()[0], 0), (pyautogui.position()[0], size_y), (0, 255, 0), 1)
+
+        #             cv2.imshow('Screenshot', img_lines)
+        #             img_lines = img
+
+        #             if cv2.waitKey(1) & 0xFF == ord('q'):
+        #                 break
+                
+        #         cv2.destroyAllWindows()
+
+        # # bind the callback function to window
+        # cv2.setMouseCallback('Screenshot', mouse_move())
+        
+        # Getting screen size
+        # 1920*1080 for current screen
+        size_x, size_y = pyautogui.size()
+        
         while True:
+            # Horizontal line
+            cv2.line(img_lines, (0, pyautogui.position()[1]), (size_x, pyautogui.position()[1]), (0, 255, 0), 1)
+
+            # Vertical Line
+            cv2.line(img_lines, (pyautogui.position()[0], 0), (pyautogui.position()[0], size_y), (0, 255, 0), 1)
+
+            cv2.imshow('Screenshot', img_lines)
+            img_lines = copy.deepcopy(img)
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        
         cv2.destroyAllWindows()
 
 
